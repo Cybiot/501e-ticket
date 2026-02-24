e1c3b0b0579f992bdc9e45520dc87980b4d0e4a8
Cybiot <marcberiot@gmail.com>
2026-02-24 19:34:52 +0100

    changement message

--- commands/ticket/panel.js
+++ commands/ticket/panel.js
@@ -15,12 +15,25 @@ module.exports = {
 
 		const embed = new EmbedBuilder()
 			.setTitle('🎫 Support')
-			.setDescription('Clique sur le bouton pour .')
+			.setDescription(`
+				tiens pour le message : 
+
+				Demander son affectation
+				Bonjour volontaire !
+
+				Si tu souhaites rejoindre notre régiment, avant toute chose, je t'invite à lire le message suivant : <#716749920488521761>
+
+				Ensuite, il te suffit de soumettre ton dossier en cliquant sur l'icône ci-dessous en répondant d'abord à quelques questions.
+
+				Une fois cela fait, un recruteur passera s'occuper de toi. N'hésite pas à laisser un petit message pour te présenter ou simplement à poser les questions que tu veux.
+
+				Prêt à rejoindre l'aventure ?
+				`)
 			.setColor(0x2b2d31);
 
 		const button = new ButtonBuilder()
 			.setCustomId('ticket_open')
-			.setLabel('Ouvrir un ticket')
+			.setLabel('Postuler')
 			.setStyle(ButtonStyle.Primary);
 
 		const row = new ActionRowBuilder().addComponents(button);
